fcde5ad1efca92beb09991712c4693ebfd003cad
BruceNotLee <bruce.lesko@outlook.com>
2018-01-28 22:50:09 -0500

    Updated with the ability to seach job postings from craigslist using a ruby scraper.

--- pull_job_postings.rb
+++
@@ -1,8 +0,0 @@
-require 'httparty'
-def jobs_search
-  job_type = 'web developer'
-  url = "https://jobs.search.gov/jobs/search.json?query=#{job_type}"
-  HTTParty.get(url).each { |job| p job['position_title'] }
-end
-
-jobs_search
--- server.rb
+++ server.rb
@@ -1,16 +1,21 @@
 require 'sinatra'
-# uses thin gem as well
-get '/' do
-  File.read(File.join('public', 'index.html'))
-  # File.read(File.join('pull_job_postings.rb', 'index.html'))
+require 'httparty'
+# get '/' do
+#   File.read(File.join('public', 'index.html'))
+#   # File.read(File.join('pull_job_postings.rb', 'index.html'))
+# end
+post '/reverse' do
+  puts params
+  erb :reversed
 end
 
-  # job_type = 'web developer'
-  require 'httparty'
-  class JobsApp < Sinatra::Base
-    def jobs_search
-      job_type = 'web developer'
-      url = "https://jobs.search.gov/jobs/search.json?query=#{job_type}"
-      HTTParty.get(url).each { |job| p job['position_title'] }
-    end
+get '/' do
+  @jobs = []
+  def jobs_search
+    job_type = params['job']
+    url = "https://jobs.search.gov/jobs/search.json?query=#{job_type}"
+    HTTParty.get(url).each { |job| @jobs << job['position_title'] }
   end
+  jobs_search
+  erb :jobslist
+end
